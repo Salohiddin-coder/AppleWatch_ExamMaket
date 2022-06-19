3d9c0142cf4d059c26555b264c81ac5e7146a8fd
Salohiddin-coder <neocrazy.gamer@gmail.com>
2022-06-19 12:44:43 +0500

    add new changes

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const elButton = document.querySelector(".menu__burger-button");
+const elMenu = document.querySelector(".menu");
+
+
+elButton.addEventListener('click', () => {
+  elMenu.classList.toggle('menu-open')
+  elButton.classList.toggle('button-active')
+})
